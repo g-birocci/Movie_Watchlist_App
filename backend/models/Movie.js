d7fd2eaebfe63e045913e18ea00d92a486461681
g-birocci <gabrielbirocci@hotmail.com>
2025-10-17 16:06:29 +0100

    update

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -1,11 +1,12 @@
-import mongoose, {now, Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const MovieSchema = new Schema({
-    _ID : Schema.Types.ObjectId,
-    title : {type: String},
-    year : {type : Number},
-    genre : {type: String},
-    watched : {type : Boolean, default: false},
-    rating : {type : Number},
-    creatAt : {type: Date, default: Date.now},
+    title: { type: String, required: true },
+    year: { type: Number, required: true },
+    genre: { type: String, required: true },
+    watched: { type: Boolean, default: false },
+    rating: { type: Number },
+    createdAt: { type: Date, default: Date.now },
 });
+
+export default mongoose.model('Movie', MovieSchema);
